7d25868f9e0eb3d11e542a63a92ea4054579c7e5
tranvantan06 <132449025+tranvantan06@users.noreply.github.com>
2024-11-08 16:16:47 +0700

    Update search.js

--- public/soundcloud/search.js
+++ public/soundcloud/search.js
@@ -2,7 +2,7 @@ const soundcloudDownloader = require("soundcloud-downloader").default;
 
 exports.name = '/soundcloud/search';
 exports.index = async (req, res, next) => {
-  if (require('../API_KEY/data/check_api_key.js').check_api_key(req, res)) return;
+ 
   const keyWord = req.query.keyword;
   if (!keyWord)
     return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
