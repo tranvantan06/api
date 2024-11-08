87f009828cd75c7b3be40b1cdab1b9b1169bf11c
tranvantan06 <132449025+tranvantan06@users.noreply.github.com>
2024-11-08 16:16:38 +0700

    Update getInfo.js

--- public/soundcloud/getInfo.js
+++ public/soundcloud/getInfo.js
@@ -2,7 +2,7 @@ const soundcloudDownloader = require("soundcloud-downloader").default;
 
 exports.name = '/soundcloud/info';
 exports.index = async (req, res, next) => {
-  if (require('../API_KEY/data/check_api_key.js').check_api_key(req, res)) return;
+
   const link = req.query.link;
   if (!link)
     return res.json({ error: 'Thiếu dữ liệu để khởi chạy chương trình ' });
